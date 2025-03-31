6cf1d6bd23889df84274fdc05ddc153341b4b91a
Agustina Luciana Gimenez <agus.luu@gmail.com>
2025-03-31 11:04:27 -0300

    Revert "comentarios borrados"
    This reverts commit a70e4c25eff3cc122d09784269eb942fee53998e.

--- pruebaGit/Controllers/HomeController.cs
+++ pruebaGit/Controllers/HomeController.cs
@@ -28,5 +28,8 @@ namespace pruebaGit.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        //comentario 2
+
     }
 }
