acd858c7e4e987c3f773427c7a7bf7e4dbccfa20
Agustina Luciana Gimenez <agus.luu@gmail.com>
2025-04-29 19:48:29 -0300

    Practica II

--- pruebaGit/Controllers/LibroController.cs
+++ pruebaGit/Controllers/LibroController.cs
@@ -5,20 +5,36 @@ namespace pruebaGit.Controllers
 {
     public class LibroController : Controller
     {
-        public IActionResult Index()
+        public IActionResult Index(string color = "white")
         {
             var autor1 = new Autor { id = 1, nombre = "George Orwell" };
             var autor2 = new Autor { id = 2, nombre = "Ray Bradbury" };
             var libros = new List<Libro>{
-
-                new Libro { id = 1, titulo = "1984", anioPublicacion = 1949, autor = autor1 },
-                new Libro { id = 2, titulo = "Fahrenheit 451", anioPublicacion =1953, autor = autor2 },
-                new Libro { id = 3, titulo = "Rebelión en la granja", anioPublicacion = 1945, autor = autor1 }
-             };
-
+                new Libro {
+                    id = 1,
+                    titulo = "1984",
+                    anioPublicacion = 1949,
+                    autor = autor1,
+                    UrlImagen = "https://upload.wikimedia.org/wikipedia/en/c/c3/1984first.jpg"
+                },
+                new Libro {
+                    id = 2,
+                    titulo = "Fahrenheit 451",
+                    anioPublicacion = 1953,
+                    autor = autor2,
+                    UrlImagen ="https://upload.wikimedia.org/wikipedia/en/d/db/Fahrenheit_451_1st_ed_cover.jpg"
+                },
+                new Libro {
+                    id = 3,
+                    titulo = "Rebelión en la granja",
+                    anioPublicacion = 1945,
+                    autor = autor1,
+                    UrlImagen = "https://upload.wikimedia.org/wikipedia/en/f/fb/Animal_Farm_-_1st_edition.jpg"
+                }
+            };
+            ViewBag.ColorFondo = color;
             return View(libros);
         }
-
         public IActionResult Detalle(int id)
         {
             var autor1 = new Autor { id = 1, nombre = "George Orwell" };
@@ -26,9 +42,9 @@ namespace pruebaGit.Controllers
 
             var libros = new List<Libro>{
                 new Libro { id = 1, titulo = "1984", anioPublicacion = 1949, autor = autor1 },
-                new Libro { id = 2, titulo = "Fahrenheit 451", anioPublicacion =1953, autor = autor2 },
+                new Libro { id = 2, titulo = "Fahrenheit 451", anioPublicacion = 1953, autor = autor2 },
                 new Libro { id = 3, titulo = "Rebelión en la granja", anioPublicacion = 1945, autor = autor1 }
-             };
+            };
 
             var libroSeleccionado = libros.FirstOrDefault(l => l.id == id);
 
--- pruebaGit/Models/Libro.cs
+++ pruebaGit/Models/Libro.cs
@@ -6,5 +6,6 @@
         public string titulo { get; set; }
         public int anioPublicacion { get; set; }
         public Autor autor { get; set; }
+        public string UrlImagen { get; set; }
     }
 }
